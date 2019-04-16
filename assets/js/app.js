fedf5d1443abf952970c389f303f606b15e4de8b
Kgor <kevin.gorham@gmail.com>
2019-04-15 19:16:40 -0700

    after class monday

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,39 @@
 
 $(document).ready(function () {
     
+    let colored = true;
+
     function starter() {
+        colored = true;
+        console.log("colored: " + colored)
         $('.cont').animate({"opacity": "1"}, 900);
         $('.name').fadeIn(1000);
+        $('.name').animate({"color": "white"}, 1000);
+        $('.recent-work-btn').animate({
+            "color":"white",
+        }, 1000)
         $('.rRect').animate({
             "width": "25%",
             "box-shadow": "0 0 20px gray"
         }, 1000, 'easeInOutCirc');
-        $('.name').animate({
-            "color": "white"
-        }, 1000);
+        
     };
+
+    function reverseAnimation() {
+        colored = false;
+        console.log("colored: " + colored)
+        $('.name').fadeIn(1000);
+        $('.name').animate({
+            "color": "gray"
+        }, 1000);
+        $('.recent-work-btn').animate({
+            "color":"gray",
+        }, 1000)
+        $('.rRect').animate({
+            "width": "0%",
+            "box-shadow": "0"
+        }, 1000, 'easeInOutCirc');  
+    }
 
     $(".action").click(function (e) {
         e.preventDefault();
@@ -20,15 +42,18 @@ $(document).ready(function () {
 
     $(".rev").click(function (e) {
         e.preventDefault();
-        $('.name').fadeIn(1000);
-        $('.name').animate({
-            "color": "gray"
-        }, 1000);
-        $('.rRect').animate({
-            "width": "0%",
-            "box-shadow": "0"
-        }, 1000, 'easeInOutCirc');   
+        reverseAnimation();  
     });
 
+    $(".recent-work-btn").click(function (e) {
+        e.preventDefault();
+        if (colored === true) {
+            reverseAnimation()
+        } else {
+            starter();
+        };
+        setTimeout(function(){ window.location.href = "./portfolio.html"; }, 1000);
+        
+    })
     starter();
 });
