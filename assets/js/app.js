7416f0e45ba7d14cdc254228a5c12f0291097844
Kgor <kevin.gorham@gmail.com>
2019-05-08 21:37:48 -0700

    added on click event to project-filter buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
 
-    if (
-        "IntersectionObserver" in window &&
-        "IntersectionObserverEntry" in window &&
-        "intersectionRatio" in window.IntersectionObserverEntry.prototype
-      ) {
-      let observer = new IntersectionObserver(entries => {
-        if (entries[0].boundingClientRect.y < 0) {
-          document.body.classList.add("header-not-at-top");
-        } else {
-          document.body.classList.remove("header-not-at-top");
-        }
-      });
-      observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
-      }
+  const filterBtns = $('.project-filter');
 
-}
+    filterBtns.on('click', function() {
+      console.log($(this));
+      filterBtns.removeClass('project-highlight');
+      $(this).addClass('project-highlight');
+    })
+
+});
