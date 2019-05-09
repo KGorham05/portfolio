389a4e46435f441a4524b1c80a1e7e954efd2777
Kgor <kevin.gorham@gmail.com>
2019-05-08 22:20:16 -0700

    added logic for hiding/showing relavant projects on click of filter buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,48 @@
 $(document).ready(function () {
 
   const filterBtns = $('.project-filter');
+  const allWork = $('.work');
+  const dasher = $('#dasher');
+  const poke = $('#poke');
+  const music = $('#music');
+  const hero = $('#hero');
+  const news = $('#news');
+  const trivia = $('#trivia');
 
-    filterBtns.on('click', function() {
-      console.log($(this));
-      filterBtns.removeClass('project-highlight');
-      $(this).addClass('project-highlight');
-    })
+  filterBtns.on('click', function () {
+    let btnId = $(this)[0].id
+    filterBtns.removeClass('project-highlight');
+    $(this).addClass('project-highlight');
+
+    switch (btnId) {
+      case 'all-btn':
+        allWork.removeClass('hidden');
+        break;
+      case 'full-stack-btn':
+        allWork.removeClass('hidden');
+        music.addClass('hidden');
+        hero.addClass('hidden');
+        trivia.addClass('hidden'); 
+        break;
+      case 'react-btn':
+        allWork.removeClass('hidden');
+        dasher.addClass('hidden');
+        music.addClass('hidden');
+        news.addClass('hidden');
+        trivia.addClass('hidden');
+        break;
+      case 'node-btn':
+        allWork.removeClass('hidden');
+        music.addClass('hidden');
+        trivia.addClass('hidden');
+        break;
+      default:
+        console.log('Something went wrong');
+    }
+
+    
+  })
+
+
 
 });
